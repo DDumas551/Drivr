e8ccebf4aca040ebc5fc552ce70529642243ccd0
DDumas551 <david.j.dumas@gmail.com>
2019-05-07 12:25:13 -0700

    Login layout test

--- drivr/src/App.js
+++ drivr/src/App.js
@@ -5,6 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header text-center">Drivr</header>
+      <p className="subHeader text-center">Rideshare Analytics</p>
+      <hr className="hr" />
+      <div className="row align-items-center btnRow">
+        <div className="col-3 offset-3">
+          <p className="loginBtn text-center">Log In</p>
+        </div>
+        <div className="col-3">
+          <p className="registerBtn text-center">Register</p>
+        </div>
+      </div>
     </div>
   );
 }
